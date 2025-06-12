8de7f464ac5fcac220b71e07d5c295158c40e357
Francisco Martinez Balian <franciscomartinezbalian@gmail.com>
2025-06-11 22:16:05 -0300

    cambio a string directo

--- red_social/nuevo_backend/src/app.module.ts
+++ red_social/nuevo_backend/src/app.module.ts
@@ -7,8 +7,16 @@ import { AutenticacionModule } from './autenticacion/autenticacion.module';
 import { PublicacionesModule } from './publicaciones/publicaciones.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 
+// @Module({
+//   imports: [ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot(process.env.MONGO_KEY!), AutenticacionModule, PublicacionesModule, UsuariosModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot(process.env.MONGO_KEY!), AutenticacionModule, PublicacionesModule, UsuariosModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot('mongodb+srv://franciscomartinezbalian:EXhOcwIAShrglx8W@cluster0.h17xfdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'), AutenticacionModule, PublicacionesModule, UsuariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
